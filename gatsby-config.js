7dc056ad34bde0cdf0d25bea684fc0cd7b1e76d7
Augusto <augustodiaz_1@hotmail.com>
2022-03-18 23:16:50 +1000

    Second trial

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+    description: "Website created following the Gatsby Official Tutorial"
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="City of Shanghai"
+        src="../images/shanghai.jpg"
+      />
+    </Layout>
+  )
+}
+
+export default IndexPage
